4e2a69cc76630baa76550e986fe840fcea739b20
Md Foysal <mdfoysal010199@gmail.com>
2026-05-02 12:08:49 +0600

    Web Authentication has been started

--- src/Layout/AuthLayout.jsx
+++ src/Layout/AuthLayout.jsx
@@ -4,7 +4,7 @@ import Navber from '../components/Navber';
 
 const AuthLayout = () => {
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto h-screen bg-[#E7E7E7]'>
 
         <Navber></Navber>
         Authentication page Layout  
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,35 +1,65 @@
+import { Link } from "react-router-dom"
 
 
 const Login = () => {
+
+    const handleLogin = event => {
+       event.preventDefault()
+        console.log(' handle Login')
+    }
+
+
     return (
-        <div >
+        <div className='container mx-auto items-center md:my-24 justify-center  w-full flex flex-col md:flex-row lg:flex-row '>
 
-            
 
-            <div className="hero bg-base-200  ">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
-                    </div>
-                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                        <div className="card-body">
-                            <fieldset className="fieldset">
-                                <label className="label">Email</label>
-                                <input type="email" className="input" placeholder="Email" />
-                                <label className="label">Password</label>
-                                <input type="password" className="input" placeholder="Password" />
-                                <div><a className="link link-hover">Forgot password?</a></div>
-                                <button className="btn btn-neutral mt-4">Login</button>
-                            </fieldset>
-                        </div>
-                    </div>
+
+
+
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div>
+                    <h2 className=' text-xl md:text-xl lg:text-2xl font-bold text-center pt-6'>
+                        <span className="text-gray-600"> Login Your Account </span>
+
+                    </h2>
                 </div>
+
+                <form
+                    onSubmit={handleLogin}
+
+                    className="card-body">
+                    <fieldset className="fieldset">
+                        <label className="label text-gray-800 font-semibold">Email</label>
+                        <input type="email" name="email" className="input  bg-red-100 border-0" placeholder="Enter your email address" />
+
+                        <label className="label text-gray-800 font-semibold">Password</label>
+                        <input type="password" name="password" className="input  bg-red-100 border-0" placeholder="Enter your password" />
+
+                        <div><a className="link link-hover text-gray-800 font-semibold">Forgot password?</a></div>
+
+
+                        <div className="form-control mt-6">
+                            <input type="submit" className="btn w-full bg-[#333333] text-white border-none text-lg hover:bg-red-800" value='Login' />
+                        </div>
+                    </fieldset>
+                </form>
+
+                <p className="text-center pb-6">Dont’t Have An Account ?<small className="text-red-600 font-medium">
+                    <Link to='/auth/register' >Register</Link>
+                </small></p>
+
             </div>
+
+
+
         </div>
+
+
+
+
+
+
+
     )
 }
 
